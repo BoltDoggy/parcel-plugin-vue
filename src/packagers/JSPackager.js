7a96574d4aa1ea64ae8f5f7cf8ad531ad8ce5837
Jasper De Moor <jasperdemoor@gmail.com>
2018-02-19 14:47:12 +0100

    update jsPackager

--- src/packagers/JSPackager.js
+++ src/packagers/JSPackager.js
@@ -15,7 +15,18 @@ class JSPackager extends JSPackagerOfficial {
         ownDebugger('start');
 
         this.first = true;
-        this.dedupe = new Map;
+        this.dedupe = new Map();
+        this.bundleLoaders = new Set();
+        this.externalModules = new Set();
+
+        if (this.options.target === 'electron') {
+            prelude =
+                `process.env.HMR_PORT=${
+                this.options.hmrPort
+                };process.env.HMR_HOSTNAME=${JSON.stringify(
+                    this.options.hmrHostname
+                )};` + prelude;
+        }
 
         await this.dest.write(prelude + '({');
     }
@@ -29,6 +40,10 @@ class JSPackager extends JSPackagerOfficial {
         if (this.options.hmr) {
             // Asset ids normally start at 1, so this should be safe.
             await this.writeModule(0, hmr.replace('{{HMR_PORT}}', this.options.hmrPort));
+            entry.push(0);
+        }
+
+        if (await this.writeBundleLoaders()) {
             entry.push(0);
         }
 
